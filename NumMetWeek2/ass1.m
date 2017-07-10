447b04680b07f35f5eb30ba72700bced5d382ae5
nikolainielsen <NikolaiNielsen@outlook.dk>
2017-07-10 20:07:57 +0200

    Diffusion seems to work using matrix multiplication

--- NumMetWeek2/ass1.m
+++ NumMetWeek2/ass1.m
@@ -5,7 +5,7 @@ L = 2*pi;
 N = 128;
 D = 0.1;
 
-x = linspace(-L/2,L/2);
+x = linspace(-L/2,L/2,N);
 h = abs(x(1)-x(2));
 
 tend = 10;
@@ -21,3 +21,27 @@ if mod(N,2) == 0
 else
     C(ceil(N/2),1) = 1/h;
 end
+
+
+% Creating the differentiation matrix
+% First we create the ones above the diagonal
+diag1 = diag(ones(N-1,1),1);
+% Then the main diagonal (-2)
+diag2 = diag(-2*ones(N,1));
+% And the ones below the diagonal
+diag3 = diag(ones(N-1,1),-1);
+T = D*dt/(h^2)*(diag1+diag2+diag3);
+
+% C(i,t+dt) = C(i,t)+T*C(:,t)
+figure
+plot(x,C(:,1))
+ax = gca;
+for i = 2:nt
+    t(i) = t(i-1)+dt;
+    C(:,i) = C(:,i-1)+T*C(:,i-1);
+    plot(x,C(:,i))
+    ax.YLim = [0 1/(2*h)];
+    drawnow
+end
+
+surf(x,t,C')
