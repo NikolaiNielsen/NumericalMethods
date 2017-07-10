2a6384927dc9070f3cbf8c3e61967c05f8c057a4
nikolainielsen <NikolaiNielsen@outlook.dk>
2017-07-10 22:33:01 +0200

    First things done. Starts error stuff

--- NumMetWeek2/ass1.m
+++ NumMetWeek2/ass1.m
@@ -5,22 +5,9 @@ L = 2*pi;
 N = 128;
 D = 0.1;
 
-x = linspace(-L/2,L/2,N);
-h = abs(x(1)-x(2));
 
-tend = 10;
-dt = h^2/(10*2*D);
-nt = ceil(tend/dt);
-t = zeros(1,nt);
+tend = 1;
 
-
-C = zeros(N,nt); % Rows are spatial, coloumns are temporal
-
-if mod(N,2) == 0
-    C(N/2:N/2+1,1) = 1/(2*h);
-else
-    C(ceil(N/2),1) = 1/h;
-end
 
 
 % Creating the differentiation matrix
@@ -30,18 +17,55 @@ diag1 = diag(ones(N-1,1),1);
 diag2 = diag(-2*ones(N,1));
 % And the ones below the diagonal
 diag3 = diag(ones(N-1,1),-1);
-T = D*dt/(h^2)*(diag1+diag2+diag3);
 
-% C(i,t+dt) = C(i,t)+T*C(:,t)
-figure
-plot(x,C(:,1))
-ax = gca;
-for i = 2:nt
-    t(i) = t(i-1)+dt;
-    C(:,i) = C(:,i-1)+T*C(:,i-1);
-    plot(x,C(:,i))
-    ax.YLim = [0 1/(2*h)];
-    drawnow
+
+Ns = 10:110;
+Ns = Ns.^2;
+dts = linspace(dt0,dt0/50,100);
+ts = zeros(size(dts));
+errdts = zeros(length(x),length(dts));
+for j = 1:length(dts)
+    
+    x = linspace(-L/2,L/2,N);
+    h = abs(x(1)-x(2));
+
+    dt0 = h^2/(2*D);
+    
+    dt = dts(j);
+    nt = ceil(tend/dt);
+    t = zeros(1,nt);
+
+
+    C = zeros(N,nt); % Rows are spatial, coloumns are temporal
+
+    if mod(N,2) == 0
+        C(N/2:N/2+1,1) = 1/(2*h);
+    else
+        C(ceil(N/2),1) = 1/h;
+    end
+
+    T = D*dt/(h^2)*(diag1+diag2+diag3);
+    
+
+    % C(i,t+dt) = C(i,t)+T*C(:,t)
+    AnalDelta = @(x,t) 1/(sqrt(2*D*t*2*pi))*exp(-(x.^2)/(4*D*t));
+    CAnal = C;
+    res = C-CAnal;
+
+
+    for i = 2:nt
+        t(i) = t(i-1)+dt;
+        C(:,i) = C(:,i-1)+T*C(:,i-1);
+        CAnal(:,i) = AnalDelta(x,t(i));
+        res(:,i) = C(:,i)-CAnal(:,i);
+    end
+    ts(j) = t(end);
+    errdts(:,j) = res(:,end);
 end
 
-surf(x,t,C')
+figure
+ax = gca;
+surf(x,dts,abs(errdts'))
+ax.ZScale = 'log';
+ax.XLim = [min(x),max(x)];
+ax.YLim = [min(dts),max(dts)];
