173356d59686022a904781e7958a4c3137f44afc
nikolainielsen <NikolaiNielsen@outlook.dk>
2017-07-11 12:01:47 +0200

    Added Von Neumann stability testing

--- NumMetWeek2/ass1.m
+++ NumMetWeek2/ass1.m
@@ -4,29 +4,40 @@ clear all; close all; clc
 % Control variables
 Simple = 0;
 ErrDt = 0;
-ErrH = 1;
-simH = 0;
+ErrH = 0;
+VonNeumann = 1;
+
+% Error as a function of dt
+simDt = 0;
+plotDt = 0;
+
+% Error as a function of N
+simH = 1;
 plotH = 1;
+
+% Von Neumann stuff
+NVtend = 10;
+NVdtMult = 1+[-1 1]*10^-2;
 
 
 % some constants
 L = 2*pi;
-N = 128;
+N0 = 128;
 D = 0.1;
 tend = 1;
 dtMult = 1;
 
 % Creating the differentiation matrix
 % First we create the ones above the diagonal
-diag1 = diag(ones(N-1,1),1);
+diag1 = diag(ones(N0-1,1),1);
 % Then the main diagonal (-2)
-diag2 = diag(-2*ones(N,1));
+diag2 = diag(-2*ones(N0,1));
 % And the ones below the diagonal
-diag3 = diag(ones(N-1,1),-1);
+diag3 = diag(ones(N0-1,1),-1);
 
 
-Ns = 100:20:1000;
-h0 = L/(N-1);
+Ns = 20:20:1000;
+h0 = L/(N0-1);
 dt0 = h0^2/(2*D);
 dts = linspace(dt0,dt0/50,100);
 ts = zeros(size(dts));
@@ -40,9 +51,8 @@ ts = zeros(size(dts));
 errdts = zeros(1,length(dts));
 errNs = zeros(1,length(Ns));
 
-
 if Simple == 1
-	x = linspace(-L/2,L/2,N);
+	x = linspace(-L/2,L/2,N0);
 	h = abs(x(1)-x(2));
 
 	dt = h^2/(dtMult*2*D);
@@ -79,10 +89,11 @@ if Simple == 1
 end
 
 if ErrDt == 1
+	N = N0;
+	x = linspace(-L/2,L/2,N);
+	h = abs(x(1)-x(2));
+	if simDt == 1
 	for j = 1:length(dts)
-
-		x = linspace(-L/2,L/2,N);
-		h = abs(x(1)-x(2));
 
 		dt = h^2/(dtMult*2*D);
 
@@ -116,6 +127,15 @@ if ErrDt == 1
 		ts(j) = t(end);
 		errdts(j) = sum(res(:,end))/N;
 		fprintf('ErrDts: run %d done\n',j)
+	end
+	save('DiffErrDt','errdts','ts','dts')
+	end
+	
+	if plotDt == 1
+		load('DiffErrDt')
+		figure
+		plot(dts,errdts,'.')
+		
 	end
 end
 
@@ -177,9 +197,42 @@ if ErrH == 1
 	end
 end
 
-% figure
-% ax = gca;
-% surf(x,dts,abs(errdts'))
-% ax.ZScale = 'log';
-% ax.XLim = [min(x),max(x)];
-% ax.YLim = [min(dts),max(dts)];
+if VonNeumann == 1
+	N = N0;
+	x = linspace(-L/2,L/2,N);
+	h = abs(x(1)-x(2));
+	tend = NVtend;
+	dts = h^2/(2*D) * NVdtMult;
+	Cend = zeros(N,2);
+	for j = 1:2
+		dt = dts(j);
+		nt = ceil(tend/dt);
+		t = zeros(1,nt);
+
+
+		C = zeros(N,nt); % Rows are spatial, coloumns are temporal
+
+		if mod(N,2) == 0
+			C(N/2:N/2+1,1) = 1/(2*h);
+		else
+			C(ceil(N/2),1) = 1/h;
+		end
+
+		T = D*dt/(h^2)*(diag1+diag2+diag3);
+
+
+		% C(i,t+dt) = C(i,t)+T*C(:,t)
+
+		for i = 2:nt
+			t(i) = t(i-1)+dt;
+			C(:,i) = C(:,i-1)+T*C(:,i-1);
+		end
+		Cend(:,j) = C(:,end);
+	end
+	figure
+	subplot(1,2,1)
+	plot(x,Cend(:,1))
+	
+	subplot(1,2,2)
+	plot(x,Cend(:,2))
+end
