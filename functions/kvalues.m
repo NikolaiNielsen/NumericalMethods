629f95096ff84e9a447dfed0a1007ee3cec15cd2
nikolainielsen <NikolaiNielsen@outlook.dk>
2017-07-12 11:17:53 +0200

    added check for odd values of N

--- functions/kvalues.m
+++ functions/kvalues.m
@@ -3,4 +3,9 @@ function k = kvalues(N)
 % Returns the values of k used in FFT and IFFT.
 % Why they look like this is a mystery and MATLAB is shit
 
-k = [0:N/2-1 0 -N/2+1:-1];
+if mod(N,2) == 0
+	k = [0:N/2-1 0 -N/2+1:-1];
+else
+	error('N /HAS/ to be even, otherwise it wont work, because MATLAB is shit')
+end
+	
