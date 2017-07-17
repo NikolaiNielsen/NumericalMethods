df817d606b844e871d3bca70f5ce3ec483245680
nikolainielsen <NikolaiNielsen@outlook.dk>
2017-07-17 15:07:28 +0200

    I think it works

--- NumMetWeek3/prisoner.m
+++ NumMetWeek3/prisoner.m
@@ -4,49 +4,76 @@ clear all; close all; clc
 % Initialize the variables needed
 
 N = 60; % Size of board (NxN)
-b = 2; % Payoff for defecting
+b = 1.85; % Payoff for defecting
 pm = [1,0;b,0]; % Payoff matrix, pm(i,j) is i's payoff for playing j
 p = 0.1; % chance of starting as defector
-
+tend = 100;
+history = cell(1,tend); 
 % Let's start the board
 s = ones(N);
+cmap = [0 0 1;...
+	    0 1 0;...
+		1 0 0;...
+		1 1 0];
 
 % populate board with defectors, and make sure there is at least 1
-count = 0;
-while sum(s(s==2)) == 0
-	sn = rand(N);
-	s(sn<=p) = 2;
-	count = count+1;
-	fprintf('Initialized board %d times\n',count)
-end
+s(30,30) = 2;
 
-a = getneighbour(s);
-
+% count = 0;
+% while sum(s(s==2)) == 0
+% 	sn = rand(N);
+% 	s(sn<=p) = 2;
+% 	count = count+1;
+% 	fprintf('Initialized board %d times\n',count)
+% end
+history{1} = s;
 % pa = zeros(N);
-payoff = zeros(N);
-hp = zeros(N);
 
-t  = circshift(s,[1,0]);
-tr = circshift(s,[1,-1]);
-tl = circshift(s,[1,1]);
-l  = circshift(s,[0,1]);
-r  = circshift(s,[0,-1]);
-bl = circshift(s,[-1,1]);
-b  = circshift(s,[-1,0]);
-br = circshift(s,[-1,-1]);
 
-% Get payoff
-% God this is ugly, and I think circshift is slower than just loops
+bc = [N,1:N,1];
+
+figure
+colormap(cmap)
+for t = 1:tend
 for i = 1:N
 for j = 1:N
-	payoff(i,j) = pm(s(i,j),tl(i,j)) + pm(s(i,j),t(i,j)) + pm(s(i,j),tr(i,j)) + ...
-				  pm(s(i,j),l(i,j))  + pm(s(i,j),s(i,j)) + pm(s(i,j),r(i,j)) + ...
-				  pm(s(i,j),bl(i,j)) + pm(s(i,j),b(i,j)) + pm(s(i,j),br(i,j));
-end
-end
-pNeigh = getneighbour(payoff);
-for i = 1:N
-	for j = 1:N
-		hp(i,j) = max(pNeigh{i,j}(:));
+	pa = 0;
+	for k = -1:1
+	for l = -1:1
+		pa = pa + pm(s(i,j),s(bc(i+k+1), bc(j+l+1)));
 	end
+	end
+	payoff(i,j) = pa;
+end
+end
+
+
+sn = s;
+hp = payoff;
+for i = 1:N
+for j = 1:N
+	for k = -1:1
+	for l = -1:1
+		index = sub2ind(size(s),bc(i+k+1),bc(j+l+1));
+% 		index = [bc(i+k+1), bc(j+l+1)];
+		if payoff(index) > hp(i,j)
+			hp(i,j) = payoff(index);
+			sn(i,j) = s(index);
+		end
+	end
+	end
+end
+end
+
+history{t} = sn;
+splot = zeros(N);
+splot(s == 1 & sn == 1) = 1;
+splot(s == 2 & sn == 1) = 2;
+splot(s == 2 & sn == 2) = 3;
+splot(s == 1 & sn == 2) = 4;
+imagesc(splot)
+drawnow
+
+s = sn;
+
 end
