bab0e464335ed0238bd7f54d464eb78ed815f395
nikolainielsen <NikolaiNielsen@outlook.dk>
2017-07-19 22:13:18 +0200

    minor shuffling around

--- NumMetWeek3/networkTest.m
+++ NumMetWeek3/networkTest.m
@@ -14,22 +14,31 @@ clc
 % % node = createnet(m,1);
 % % node2 = deletenode(node,2);
 % 
-% % Let's create a random, symmetric matrix
-% p = 0.1;
-% n = 10;
-% 
-% a = randmat(n,p);
+% Let's create a random, symmetric matrix
+m = 10;
+n = 1000;
+p = (1+m/n)/2;
+
+m = 100;
+avg = zeros(1,m);
+for i = 1:m
+a = adjmatrix(n,p);
+b = sum(a);
+avg(i) = mean(b);
+end
+figure
+plot(avg)
+mean(avg)
 % node = createnet(a,0);
 % [node1,l1] = bfs(node);
-% [node2,l2] = bfs2(node);
 % g = graph(a);
 % plot(g);
 
 
 %% Let's try some real world.
 
-data = importdata('net/email.txt');
-data = data(:,1:2);
-data = unique(sort(data,2),'rows');
-g = graph(data(:,1),data(:,2));
-plot(g)
+% data = importdata('net/email.txt');
+% data = data(:,1:2);
+% data = unique(sort(data,2),'rows');
+% g = graph(data(:,1),data(:,2));
+% plot(g)
