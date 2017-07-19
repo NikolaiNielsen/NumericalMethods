fb86568890fdc21b7a6da86ffd03a6b71703ab16
nikolainielsen <NikolaiNielsen@outlook.dk>
2017-07-19 11:38:48 +0200

    Added stacked area graph

--- NumMetWeek3/prisoner.m
+++ NumMetWeek3/prisoner.m
@@ -3,21 +3,24 @@ clear all; close all; clc
 
 % Initialize the variables needed
 
-N = 60; % Size of board (NxN)
+N = 75; % Size of board (NxN)
 b = 1.85; % Payoff for defecting
 pm = [1,0;b,0]; % Payoff matrix, pm(i,j) is i's payoff for playing j
 p = 0.1; % chance of starting as defector
-tend = 500;
+tend = 1000;
+plotting = 1;
+
+
 history = cell(1,tend); 
 % Let's start the board
 s = ones(N);
 cmap = [0 0 1;...
 	    0 1 0;...
-		1 0 0;...
-		1 1 0];
+		1 1 0;...
+		1 0 0];
 
 % populate board with defectors, and make sure there is at least 1
-s(3,3) = 2;
+s(38,38) = 2;
 
 % count = 0;
 % while sum(s(s==2)) == 0
@@ -28,36 +31,16 @@ s(3,3) = 2;
 % end
 history{1} = s;
 % pa = zeros(N);
+count = zeros(4,tend);
 
 
 bc = [N,1:N,1];
 
-% figure
-% colormap(cmap)
-% 
-% splot = zeros(N);
-% splot(s == 1) = 1;
-% splot(s == 2) = 3;
-% imagesc(splot, [1,4])
-% drawnow
-% 
-% t  = circshift(s,[1,0]);
-% tr = circshift(s,[1,-1]);
-% tl = circshift(s,[1,1]);
-% l  = circshift(s,[0,1]);
-% r  = circshift(s,[0,-1]);
-% bl = circshift(s,[-1,1]);
-% b  = circshift(s,[-1,0]);
-% br = circshift(s,[-1,-1]);
-% pt = pm(sub2ind([2,2],s,t));
-% 
-
-% for i = -1:1
-% 	for j = -1:1
-% 		p = p+pm(sub2ind([2,2],s,circshift(s,[i,j])));
-% 	end
-% end
-
+splot = zeros(N);
+splot(s == 1) = 1;
+splot(s == 2) = 4;
+count(1,1) = sum(sum(splot == 1));
+count(4,1) = sum(sum(splot == 4));
 
 
 
@@ -101,19 +84,51 @@ for j = 1:N
 	end
 end
 end
-
+% 
 % figure
 % colormap(cmap)
-% history{t} = sn;
-% splot = zeros(N);
-% splot(s == 1 & sn == 1) = 1;
-% splot(s == 2 & sn == 1) = 2;
-% splot(s == 2 & sn == 2) = 3;
-% splot(s == 1 & sn == 2) = 4;
-% imagesc(splot, [1,4])
-% drawnow
+history{t} = sn;
+splot = zeros(N);
+splot(s == 1 & sn == 1) = 1;
+splot(s == 2 & sn == 1) = 2;
+splot(s == 1 & sn == 2) = 3;
+splot(s == 2 & sn == 2) = 4;
+count(1,t) = sum(sum(splot == 1));
+count(2,t) = sum(sum(splot == 2));
+count(3,t) = sum(sum(splot == 3));
+count(4,t) = sum(sum(splot == 4));
 
 s = sn;
 % disp(t)
 
 end
+
+if plotting == 1
+	splot = zeros(N);
+	splot(s == 1) = 1;
+	splot(s == 2) = 4;
+	
+	figure
+	colormap(cmap)
+	imagesc(splot, [1,4])
+	drawnow
+	
+	for t = 2:tend
+		s = history{t-1};
+		sn = history{t};
+		splot = zeros(N);
+		splot(s == 1 & sn == 1) = 1;
+		splot(s == 2 & sn == 1) = 2;
+		splot(s == 1 & sn == 2) = 3;
+		splot(s == 2 & sn == 2) = 4;
+		imagesc(splot,[1,4])
+		drawnow
+	end
+	
+end
+
+figure
+colormap(cmap)
+area(count')
+ax = gca;
+ax.YLim = [0 N^2];
