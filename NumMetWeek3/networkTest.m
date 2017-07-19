3fbb494a5c40f5fb202cde7c4ce83a7d9b6ac0ff
nikolainielsen <NikolaiNielsen@outlook.dk>
2017-07-19 15:58:42 +0200

    I got tired so started work on real life networks

--- NumMetWeek3/networkTest.m
+++ NumMetWeek3/networkTest.m
@@ -3,20 +3,33 @@ close all
 clc
 
 
-m = [0 1 1 1 0 0 0;...
-	 1 0 0 0 1 0 0;...
-	 1 0 0 1 0 0 0;...
-	 1 0 1 0 0 0 0;...
-	 0 1 0 0 0 0 0;...
-	 0 0 0 0 0 0 1;...
-	 0 0 0 0 0 1 0];
+% m = [0 1 1 1 0 0 0;...
+% 	 1 0 0 0 1 0 0;...
+% 	 1 0 0 1 0 0 0;...
+% 	 1 0 1 0 0 0 0;...
+% 	 0 1 0 0 0 0 0;...
+% 	 0 0 0 0 0 0 1;...
+% 	 0 0 0 0 0 1 0];
+% 
+% % node = createnet(m,1);
+% % node2 = deletenode(node,2);
+% 
+% % Let's create a random, symmetric matrix
+% p = 0.1;
+% n = 10;
+% 
+% a = randmat(n,p);
+% node = createnet(a,0);
+% [node1,l1] = bfs(node);
+% [node2,l2] = bfs2(node);
+% g = graph(a);
+% plot(g);
 
-node = createnet(m,1);
-node2 = deletenode(node,2);
 
-% Let's create a random, symmetric matrix
-p = 0.5;
-n = 200;
+%% Let's try some real world.
 
-a = randmat(n,p);
-node = createnet(a,1);
+data = importdata('net/email.txt');
+data = data(:,1:2);
+data = unique(sort(data,2),'rows');
+g = graph(data(:,1),data(:,2));
+plot(g)
